43e3cb9c7dbb8c64d6fcfa55eb3142c43ba8f5ff
Johan Paul <johan.paul@gmail.com>
2012-06-23 22:05:30 +0300

    WP7: Basic UI views and components
    Here are the main pivot view and their main components. This should be a
    good starting point for some real coding...

---
+++ wp7/Podcatcher/Podcatcher/CustomControls/PodcastPivotItemHeader.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Podcatcher
+{
+	public partial class PodcastPivotItemHeader : UserControl
+	{
+		public PodcastPivotItemHeader()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+
+        private String _headerText;
+        public String HeaderText
+        {
+            get
+            {
+                return _headerText;
+            }
+
+            set
+            {
+                _headerText = value;
+                this.PivotHeader.Text = _headerText;
+            }
+        }
+
+        private String _altText;
+        public String AltText
+        {
+            get 
+            {
+                return _altText;
+            }
+
+            set
+            {
+                _altText = value;
+                this.PivotAltLine.Text = _altText;
+            }
+        }
+
+    }
+}
