97ed3373778750fd27bde026841102fea4d86342
Johan Paul <johan.paul@gmail.com>
2012-06-24 16:14:49 +0300

    Removed sample data by Visual Studio.

--- wp7/Podcatcher/Podcatcher/Views/MainView.xaml.cs
+++ wp7/Podcatcher/Podcatcher/Views/MainView.xaml.cs
@@ -20,12 +20,10 @@ namespace Podcatcher
         {
             InitializeComponent();
 
-            // Set the data context of the listbox control to the sample data
             DataContext = PodcastSubscriptionsModel.Instance;
             this.Loaded += new RoutedEventHandler(MainPage_Loaded);
         }
 
-        // Load data for the ViewModel Items
         private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
         }
