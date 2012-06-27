45a6657958289af7d4011a9f8c1271bff0a72c4c
Johan Paul <johan.paul@cybercom.com>
2012-06-27 13:53:03 +0300

    WP7: Added "No subscriptions" text to front page (when needed).
    In this commit, I also added a new Converters namespace that is used in this case to determine if the text is visible or not.

---
+++ wp7/Podcatcher/Podcatcher/Converters/Converters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Data;
+using Podcatcher.ViewModels;
+using System.Globalization;
+using System.Collections.ObjectModel;
+
+namespace Podcatcher.Converters
+{
+    public class NoSubscriptionsVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ObservableCollection<PodcastSubscriptionModel> model = value as ObservableCollection<PodcastSubscriptionModel>;
+            if (model == null
+                || model.Count < 1)
+            {
+                return Visibility.Visible;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
