3c7dcd0c2b74a740c33ec2ba2382d98f715755f3
Johan Paul <johan.paul@cybercom.com>
2012-06-27 10:43:43 +0300

    Add "Remove" context menu for list items in postcast subscriptions list.

--- wp7/Podcatcher/Podcatcher/CustomControls/PodcastItem.xaml.cs
+++ wp7/Podcatcher/Podcatcher/CustomControls/PodcastItem.xaml.cs
@@ -7,6 +7,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using Microsoft.Phone.Controls;
 
 namespace Podcatcher
 {
@@ -17,5 +19,11 @@ namespace Podcatcher
 			// Required to initialize variables
 			InitializeComponent();
 		}
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            PodcastSubscriptionModel subscriptionToDelete = (sender as MenuItem).DataContext as PodcastSubscriptionModel;
+            Debug.WriteLine("Delete index.");
+        }
 	}
 }
