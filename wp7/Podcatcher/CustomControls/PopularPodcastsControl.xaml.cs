8798e407153f82520137d34eb3a611fc06385b51
Johan Paul <johan.paul@cybercom.com>
2012-07-06 11:54:39 +0300

    WP7: Started working on the popular podcasts page.

---
+++ wp7/Podcatcher/CustomControls/PopularPodcastsControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Podcatcher.CustomControls
+{
+    public partial class PopularPodcastsControl : UserControl
+    {
+        public PopularPodcastsControl()
+        {
+            InitializeComponent();
+            
+            Loaded += PageLoaded;
+        }
+
+        void PageLoaded(object sender, RoutedEventArgs e)
+        {
+            WebClient wc = new WebClient();
+            wc.DownloadStringCompleted += new DownloadStringCompletedEventHandler(wc_DownloadStringCompleted);
+            wc.DownloadStringAsync(new Uri("http://gpodder.net/toplist/15.xml"));
+        }
+
+        void wc_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            XDocument xmlResult = XDocument.Parse(e.Result);
+        }
+    }
+}
