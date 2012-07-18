ecb60b1615bb69ebb2804be3c2beeaff2b957f65
Johan Paul <johan.paul@gmail.com>
2012-07-18 17:56:15 +0300

    WP7: Uups, adding GPOdderResultModel file.

---
+++ wp7/Podcatcher/ViewModels/GPodderResultModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Podcatcher.ViewModels
+{
+    public class GPodderResultModel
+    {
+        public Uri PodcastLogoUrl
+        {
+            get;
+            set;
+        }
+
+        public String PodcastName
+        {
+            get;
+            set;
+        }
+
+        public String PodcastUrl
+        {
+            get;
+            set;
+        }
+    }
+}
