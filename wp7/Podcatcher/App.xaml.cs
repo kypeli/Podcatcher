b5347c2fb2a7a65a9ee391d80baf220936b5c840
Johan Paul <johan.paul@gmail.com>
2012-09-16 12:05:28 +0300

    Enable again storing SQL state at app exit and deactivation.

--- wp7/Podcatcher/App.xaml.cs
+++ wp7/Podcatcher/App.xaml.cs
@@ -118,14 +118,14 @@ namespace Podcatcher
         // This code will not execute when the application is closing
         private void Application_Deactivated(object sender, DeactivatedEventArgs e)
         {
-            //PodcastSqlModel.getInstance().SubmitChanges();
+            PodcastSqlModel.getInstance().SubmitChanges();
         }
 
         // Code to execute when the application is closing (eg, user hit Back)
         // This code will not execute when the application is deactivated
         private void Application_Closing(object sender, ClosingEventArgs e)
         {
-//            PodcastSqlModel.getInstance().SubmitChanges();
+            PodcastSqlModel.getInstance().SubmitChanges();
         }
 
         // Code to execute if a navigation fails
