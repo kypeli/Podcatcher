0310b42e78f9ef0e31891cf969404561ecdebae4
Johan Paul <johan.paul@gmail.com>
2012-12-18 16:03:38 +0200

    Add missing PodcastPlayerView.

---
+++ wp7/Podcatcher/Views/PodcastPlayerView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace Podcatcher.Views
+{
+    public partial class PodcastPlayerView : PhoneApplicationPage
+    {
+        public PodcastPlayerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
