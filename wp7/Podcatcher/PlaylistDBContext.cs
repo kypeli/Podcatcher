06e869d76e95f63a725827b65e36d8ed3b55fdb6
Johan Paul <johan.paul@gmail.com>
2013-03-30 21:58:26 +0200

    Add missing files.

---
+++ wp7/Podcatcher/PlaylistDBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Data.Linq;
+using Podcatcher.ViewModels;
+
+namespace Podcatcher
+{
+    public class PlaylistDBContext : DataContext
+    {
+        private const string m_connection = "Data Source=isostore:/PodcatcherPlaylists.sdf";
+        
+        public Table<PlaylistItem> Playlist;
+
+        public PlaylistDBContext()
+            : base(m_connection)
+        {
+            if (!DatabaseExists())
+            {
+                CreateDatabase();
+            }
+
+            Playlist = GetTable<PlaylistItem>();
+            // TODO: Handle scheme changes.                    
+        }
+
+    }
+}
---
+++ wp7/Podcatcher/ViewModels/PlaylistItem.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace Podcatcher.ViewModels
+{
+    [Table]
+    public class PlaylistItem
+    {
+
+#region properties
+        [Column(IsPrimaryKey = true, CanBeNull = false, IsDbGenerated = true)]
+        public int ItemId
+        {
+            get;
+            set;
+        }
+
+        [Column]
+        public int OrderNumber
+        {
+            get;
+            set;
+        }
+
+        [Column]
+        public string PodcastName
+        {
+            get;
+            set;
+        }
+
+        [Column]
+        public string PodcastLogoLocation
+        {
+            get;
+            set;
+        }
+
+        [Column]
+        public string EpisodeName
+        {
+            get;
+            set;
+        }
+
+        [Column]
+        public string EpisodeLocation
+        {
+            get;
+            set;
+        }
+
+        [Column]
+        public int EpisodeId
+        {
+            get;
+            set;
+        }
+
+        [Column(DbType = "BIT DEFAULT 0 NOT NULL")]
+        public bool IsCurrent
+        {
+            get;
+            set;
+        }
+#endregion
+
+    }
+}
