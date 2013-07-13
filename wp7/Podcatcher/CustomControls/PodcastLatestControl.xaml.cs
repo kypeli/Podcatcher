5b50f7b21c1a1f61f998748cc5b372f1032cdbae
Johan Paul <johan.paul@gmail.com>
2013-07-13 12:32:06 +0300

    Add missing files.

---
+++ wp7/Podcatcher/CustomControls/PodcastLatestControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Podcatcher.ViewModels;
+
+namespace Podcatcher
+{
+    public partial class PodcastLatestControl : UserControl
+    {
+        public PodcastLatestControl()
+        {
+            InitializeComponent();
+        }
+
+        private void LatestEpisodeTapped(object sender, GestureEventArgs e)
+        {
+            PodcastEpisodeModel episode = DataContext as PodcastEpisodeModel;
+            if (episode == null)
+            {
+                App.showNotificationToast("You don't subscribe to the podcast anymore.");
+                return;
+            }
+
+            PodcastPlaybackManager.getInstance().play(episode);
+        }
+    }
+}
