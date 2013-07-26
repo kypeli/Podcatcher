afc571a786d5ce9f8cf420fdb948c43e516f7ad6
Johan Paul <johan.paul@gmail.com>
2013-07-27 00:05:39 +0300

    Fill the categories with actual stuff.

--- wp7/Podcatcher/CustomControls/BrowsePodcastsControl.xaml.cs
+++ wp7/Podcatcher/CustomControls/BrowsePodcastsControl.xaml.cs
@@ -19,24 +19,28 @@ namespace Podcatcher.CustomControls
     public partial class BrowsePodcastsControl : UserControl
     {
         private const String GPODDER_API_BASEURL = "https://gpodder.net/api/2/";
-        private const int NUMBER_OF_PODCASTS_PER_GROUP = 25;
+        private const int NUMBER_OF_PODCASTS_PER_GROUP = 40;
 
         public BrowsePodcastsControl()
         {
             AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Arts", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/arts/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
-            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "CBS" });                 // https://gpodder.net/directory/CBC
-            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Comedy" });              // https://gpodder.net/directory/comedy
-            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Computer Science" });
-            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Drama" });
-            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Gadgets" });
-            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "News" });
-            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Religion" });
-            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Politics" });
-            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Technology" });          // https://gpodder.net/directory/technology
-            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "TV" });
-            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Science" });             // https://gpodder.net/directory/Science
-            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Sports" });              // https://gpodder.net/api/2/tag/Sports%20&%20Recreation/25.json
-            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Skepticism" });
+            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "BBC", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/bbc/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
+            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "CBS", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/cbs/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
+            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Comedy", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/comedy/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
+            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Computer Science", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/Computer Science/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
+            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Drama", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/drama/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
+            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Gadgets", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/gadgets/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
+            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "News", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/News/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
+            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Religion", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/religion/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
+            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Politics", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/Politics/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
+            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Talk Radio", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/Talk Radio/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
+            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Technology", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/technology/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
+            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "TV", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/tv/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
+            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Science", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/Science/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
+            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Sports", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/Sports%20&%20Recreation/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
+            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Skepticism", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/skepticism/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
+            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Literature", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/Literature/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
+            AddSubscription.PodcastGroups.Add(new BrowsePodcastsGroupModel { Name = "Video Games", RestAPIUrl = new Uri(GPODDER_API_BASEURL + "tag/Video Games/" + NUMBER_OF_PODCASTS_PER_GROUP + ".json") });
             
             InitializeComponent();
             Loaded += PageLoaded;
