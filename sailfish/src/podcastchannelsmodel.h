56b127f66bca26606c1fe21e94e02b9aa2acfd45
Moritz Carmesin <moritz@carmesinus.de>
2016-08-19 20:33:32 +0200

    update authors

--- sailfish/src/podcastchannelsmodel.h
+++ sailfish/src/podcastchannelsmodel.h
@@ -1,19 +1,19 @@
 /**
- * This file is part of Podcatcher for N9.
+ * This file is part of Podcatcher for Sailfish OS.
  * Author: Johan Paul (johan.paul@gmail.com)
  *
- * Podcatcher for N9 is free software: you can redistribute it and/or modify
+ * Podcatcher for Sailfish OS is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Podcatcher for N9 is distributed in the hope that it will be useful,
+ * Podcatcher for Sailfish OS is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Podcatcher for N9.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Podcatcher for Sailfish OS.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef PODCASTCHANNELSMODEL_H
 #define PODCASTCHANNELSMODEL_H
